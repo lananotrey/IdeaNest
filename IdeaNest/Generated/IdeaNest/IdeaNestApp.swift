a0a7c4747bde00319803eb134422558de749b5e6
Леха Кайфарик <workcar@MacBook-Air-Leha.local>
2025-04-14 13:52:46 -0700

    Undefined symbol: _main Linker command failed with exit code 1 (use -v to see invocation)

--- IdeaNest/Generated/IdeaNest/IdeaNestApp.swift
+++ IdeaNest/Generated/IdeaNest/IdeaNestApp.swift
@@ -1,17 +1,10 @@
-import Foundation
+import SwiftUI
 
-struct Idea: Identifiable, Codable {
-    var id: UUID
-    var title: String
-    var description: String
-    var isFavorite: Bool
-    var createdDate: Date
-    
-    init(id: UUID = UUID(), title: String, description: String, isFavorite: Bool = false, createdDate: Date = Date()) {
-        self.id = id
-        self.title = title
-        self.description = description
-        self.isFavorite = isFavorite
-        self.createdDate = createdDate
+@main 
+struct IdeaNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
 }
--- IdeaNest/IdeaNestApp.swift
+++ IdeaNest/IdeaNestApp.swift
@@ -1,14 +1,17 @@
-import SwiftUI
-import FirebaseCore
-import FirebaseRemoteConfig
+import Foundation
 
-@main
-struct IdeaNestApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+struct Idea: Identifiable, Codable {
+    var id: UUID
+    var title: String
+    var description: String
+    var isFavorite: Bool
+    var createdDate: Date
     
-    var body: some Scene {
-        WindowGroup {
-            RemoteScreen()
-        }
+    init(id: UUID = UUID(), title: String, description: String, isFavorite: Bool = false, createdDate: Date = Date()) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.isFavorite = isFavorite
+        self.createdDate = createdDate
     }
 }
