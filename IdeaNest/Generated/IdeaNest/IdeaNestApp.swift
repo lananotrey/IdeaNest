a231abd2d4ce3cc58e0bd4e56ef55e851da494c8
Леха Кайфарик <workcar@MacBook-Air-Leha.local>
2025-04-14 13:53:06 -0700

    /Users/workcar/Documents/gen project/IdeaNest/IdeaNest/IdeaNestApp.swift:3:8 Invalid redeclaration of 'Idea'

--- IdeaNest/Generated/IdeaNest/IdeaNestApp.swift
+++ IdeaNest/Generated/IdeaNest/IdeaNestApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main 
 struct IdeaNestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
